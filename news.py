90eb138ce8934eed7ab07dbebb85c782037752cd
Alexey <shalin.2001@bk.ru>
2020-12-30 13:03:48 +0700

    asdf

--- news.py
+++ news.py
@@ -86,20 +86,20 @@ def main():
         for item in data:
             if find_news(news, item['news_url']) == -1:
                 news.append(item)
-                message = f"\n============= NEWS =============\nSentiment:{item['sentiment']}\nTitle:{item['title']}\nText:{item['text']}"
-                print(message)
-                #sms(message)
+                text = f"\n============= NEWS =============\nSentiment:{item['sentiment']}\nTitle:{item['title']}\nText:{item['text']}"
+                print(text)
+                #sms(text)
             #winsound.Beep(freq, duration)
             
 
         time.sleep(60)
 
 
-def sms(message): 
+def sms(text): 
     message = twilio_client.messages.create(
         to="+12513090696", 
         from_=twilio_phone_number,
-        body=message
+        body=text
 
     print("Twilio message id: " + message.sid)
 
